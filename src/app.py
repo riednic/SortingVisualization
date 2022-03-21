902931bc5839425095f2a036357fa8d75f39f8a5
Nico Riedl <riedl.nico@yahoo.com>
2022-03-21 16:13:46 -0700

    Moved to pygame

--- src/app.py
+++ src/app.py
@@ -1,14 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QWidget
-
-
 def main():
-    app = QApplication([])
-    window = QWidget()
-    window.setWindowTitle('Sorting Visualization')
-    window.setGeometry(100, 100, 280, 80)
-    window.move(60, 15)
-    window.show()
-    app.exec()
+    pass
 
 
 if __name__ == "__main__":
